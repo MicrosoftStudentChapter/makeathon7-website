f037613c5869d840034e15e481137f5b7ce9aa49
Keshav Aggarwal <keshavaggarwal191106@gmail.com>
2025-01-18 22:56:32 +0530

    Test Changes
    ABC

--- src/components/Sponsors/Pages.jsx
+++ src/components/Sponsors/Pages.jsx
@@ -3,30 +3,59 @@ import styles from './Pages.module.css'
 
 const Pages = () => {
   return (
-    <div className={styles.container}>
+    <div className={styles.main}>
+      <div className={styles.container}>
 
-        <div className={styles.row}>
+          <div className={styles.row}>
+            
+            <div classname={styles.column}>
+              <div className={styles.sides1}><h1 class={styles.invi}>&nbsp;</h1></div>
+              <div className={styles.sides1}><h1 class={styles.invi}>&nbsp;</h1></div>
+              <div className={styles.sides1}><h1 class={styles.invi}>&nbsp;</h1></div>
+              <div className={styles.sideleft}><h1 class={styles.invi}>&nbsp;</h1></div>
+            </div>
 
-          <div className={styles.filespagebgleft}>
-            <div className={styles.filespageleft1}>
-              <div className={styles.filespageleft2}>
-                <h1 align="center">TITLE</h1>
-                <h1 align="center">COMPANY NAME</h1>
+            <div className={styles.filespagebgleft}>
+              <div className={styles.filespageleft1}>
+                <div className={styles.filespageleft2}>
+                  <h1 class={styles.invi}>&nbsp;</h1>
+                  <h1 align="center">TITLE</h1>
+                  <div class={styles.Polaroid}> 
+                    <div class={styles.Inside}>
+                      <div class={styles.InsidePolaroid}></div>
+                    </div>
+                  </div>
+                  <h1 align="center">COMPANY NAME</h1>
+                </div>
               </div>
             </div>
-          </div>
 
-          <div className={styles.filespagebgright}>
-            <div className={styles.filespageright1}>
-              <div className={styles.filespageright2}>
-                <h1 align="center">SPONSORS</h1>
-                <h1 align="center">COMPANY NAME</h1>
+            <div className={styles.slit}><h1 class={styles.invi}>.</h1></div>
+
+            <div className={styles.filespagebgright}>
+              <div className={styles.filespageright1}>
+                <div className={styles.filespageright2}>
+                  <h1 class={styles.invi}>&nbsp;</h1>
+                  <h1 align="center">SPONSORS</h1>
+                  <div class={styles.Polaroid}> 
+                    <div class={styles.Inside}>
+                      <div class={styles.InsidePolaroid}></div>
+                    </div>  
+                  </div>
+                  <h1 align="center">COMPANY NAME</h1>
+                </div>
               </div>
             </div>
+
+            <div classname={styles.column}>
+              <div className={styles.sides1}><h1 class={styles.invi}>&nbsp;</h1></div>
+              <div className={styles.sides1}><h1 class={styles.invi}>&nbsp;</h1></div>
+              <div className={styles.sides1}><h1 class={styles.invi}>&nbsp;</h1></div>
+              <div className={styles.sideright}><h1 class={styles.invi}>&nbsp;</h1></div>
+            </div>
+
           </div>
-
-        </div>
-
+      </div>
     </div>
   )
 }
