54dbd62c0a00fc6406245342603759038b7e093a
Dhruv Srivastava <dhruvsri37@gmail.com>
2025-01-19 12:40:44 +0530

    Made the polaroid element

--- src/components/Sponsors/polaroid.jsx
+++ src/components/Sponsors/polaroid.jsx
@@ -1,58 +1,19 @@
 import React from 'react';
-import { Image, View, StyleSheet } from 'react-native';
+import styles from './Polaroid.module.css'; // Import the CSS module
 
 const SmallBox = () => {
   return (
-    
-    <div style={styles.box}>
-      <div style = {styles.box2}>
-       <div style = {styles.box3}>
-
-       <Image style = {{
-          width: 180,
-          height: 180,
-
-        }}source={require('./facebook.png')} />
-
-       </div>
-      
-
+    <div className={styles.polaroid}>
+      <div className={styles.blackBox}>
+        <img
+          className={styles.image}
+          src="/Sponsors/facebook.png" // Use the relative path from the public directory
+          alt="Facebook"
+        />
       </div>
-
+      <div className={styles.caption}>Facebook</div>
     </div>
   );
 };
-
-const styles = StyleSheet.create({
-  box: {
-    width: 250,
-    height: 300,
-    backgroundColor: '#ECECEB',
-    //for the positioning of the box
-    marginLeft: 50,
-    marginTop: 100,
-  },
-
-  box2: {
-    width: 230,
-    height:230,
-    backgroundColor: 'black',
-    //for the positioning of the box
-    marginLeft: 10,
-    marginTop: 10,
-  
-  },
-
-  box3: {
-    width: 230,
-    height:230,
-    //for the positioning of the box
-    marginLeft: 25,
-    paddingTop: 22,
-  
-  },
-
-
-});
 
 export default SmallBox;
--- src/pages/Sponsors.jsx
+++ src/pages/Sponsors.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styles from '../styles/Sponsors.module.css'
 import Pages from '../components/Sponsors/Pages'
+import SmallBox from '../components/Sponsors/polaroid'
 
 const Sponsors = () => {
   return (
@@ -8,7 +9,8 @@ const Sponsors = () => {
     <div className={styles.wrapper}>
       <div className={styles.container}>
         <img className={styles.logo} src="/public/mlsclogo.png" alt="MLSC Logo" />
-        <Pages />
+        {/*<Pages />*/}
+        <SmallBox />
       </div>
     </div>
     </>
