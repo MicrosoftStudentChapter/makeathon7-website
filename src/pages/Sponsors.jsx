e71d76b6901a69c4e05862497eb4921a71e54574
Dhruv Srivastava <dhruvsri37@gmail.com>
2025-01-22 16:22:40 +0530

    Tried switching components when scrolling. CSS needs fixing.

--- src/pages/Sponsors.jsx
+++ src/pages/Sponsors.jsx
@@ -1,28 +1,46 @@
-import React from 'react'
-import styles from '../styles/Sponsors.module.css'
-
-import PagesT from '../components/SponsorsPageInside/PagesT.jsx'
-import PagesP from '../components/SponsorsPageInside/PagesP.jsx'
-import PagesA from '../components/SponsorsPageInside/PagesA.jsx'
-
-import ClosedFile from "../components/Sponsors/ClosedFile";
+import React, { useState, useEffect } from 'react';
+import styles from '../styles/Sponsors.module.css';
+import PagesT from '../components/SponsorsPageInside/PagesT.jsx';
+import PagesP from '../components/SponsorsPageInside/PagesP.jsx';
+import PagesA from '../components/SponsorsPageInside/PagesA.jsx';
+import ClosedFile from '../components/Sponsors/ClosedFile';
 
 const Sponsors = () => {
+  const [scrollPosition, setScrollPosition] = useState(0);
+
+  const handleScroll = () => {
+    setScrollPosition(window.scrollY);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  let content;
+  if (scrollPosition > 150) {
+    content = <PagesA />;
+  } else if (scrollPosition > 100) {
+    content = <PagesP />;
+  } else if (scrollPosition > 50) {
+    content = <PagesT />;
+  } else {
+    content = <ClosedFile />;
+  }
+
   return (
     <>
-    <div className={styles.wrapper}>
-      <div className={styles.container}>
-        <img className={styles.logo} src="/mlsclogo.png" alt="MLSC Logo" />
-        <img className={styles.menu} src="/Sponsors/menu.png" alt="MLSC Logo" />
-        {/* <Pages />  */}
-        <PagesT />   
-        <PagesP />     
-        <PagesA />
-        <ClosedFile/>
+      <div className={`${styles.wrapper} ${scrollPosition > 50 ? styles.scrolled : ''}`}>
+        <div className={styles.container}>
+          <img className={styles.logo} src="/mlsclogo.png" alt="MLSC Logo" />
+          <img className={styles.menu} src="/Sponsors/menu.png" alt="Menu" />
+          <div className={styles.fade}>{content}</div>
+        </div>
       </div>
-    </div>
     </>
-  )
-}
+  );
+};
 
-export default Sponsors
+export default Sponsors;
