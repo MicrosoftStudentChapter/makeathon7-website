be6b470800a7630c349a27fb4801437e36aab0b7
Keshav Aggarwal <keshavaggarwal191106@gmail.com>
2025-01-22 17:58:09 +0530

    Put it in center and made the file stay at a fixed posiiton
    -Keshav
    need some edits thou

--- src/pages/Sponsors.jsx
+++ src/pages/Sponsors.jsx
@@ -20,11 +20,11 @@ const Sponsors = () => {
   }, []);
 
   let content;
-  if (scrollPosition > 150) {
+  if (scrollPosition > 600) {
     content = <PagesA />;
-  } else if (scrollPosition > 100) {
+  } else if (scrollPosition > 400) {
     content = <PagesP />;
-  } else if (scrollPosition > 50) {
+  } else if (scrollPosition > 200) {
     content = <PagesT />;
   } else {
     content = <ClosedFile />;
@@ -32,11 +32,15 @@ const Sponsors = () => {
 
   return (
     <>
-      <div className={`${styles.wrapper} ${scrollPosition > 50 ? styles.scrolled : ''}`}>
-        <div className={styles.container}>
-          <img className={styles.logo} src="/mlsclogo.png" alt="MLSC Logo" />
-          <img className={styles.menu} src="/Sponsors/menu.png" alt="Menu" />
-          <div className={styles.fade}>{content}</div>
+      <div className={`${styles.wrapper} ${scrollPosition > 100 ? styles.scrolled : ''}`}>
+        <div className={styles.container}>  
+
+          <div className={styles.AbsoluteFixed}>
+            <img className={styles.logo} src="/mlsclogo.png" alt="MLSC Logo" />
+            <img className={styles.menu} src="/Sponsors/menu.png" alt="Menu" />
+            <div className={styles.fade}>{content}</div>
+          </div>
+
         </div>
       </div>
     </>
